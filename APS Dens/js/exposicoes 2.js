d4a21d1e81a5cf8a3d9e06ac490274785f51328b
Mylena-Costa <55564217+Mylena-Costa@users.noreply.github.com>
2021-04-08 14:06:24 -0300

    Add files via upload

---
+++ APS Dens/js/exposicoes 2.js
@@ -0,0 +1,16 @@
+<html>
+    <head>
+    <script>
+    $(function () {
+        $(".gallery").fancybox({
+            option
+        });
+    });
+</script>
+<a href="image.jpg" class="gallery" data-fancybox data-caption="My caption">
+	        <img src="thumbnail.jpg" alt="" />
+</a>
+</head>
+<body>
+                </body>
+</html>
